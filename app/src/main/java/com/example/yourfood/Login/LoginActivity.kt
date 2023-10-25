a32e5f6051ec609f45332531a928a1915888c818
Vikas Kumar Gupta <vikasgup074@gmail.com>
2023-10-25 18:18:46 +0530

    Second Page

---
+++ app/src/main/java/com/example/yourfood/Login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yourfood.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.yourfood.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
