b1cbf18a744cbd92135266e0f346565ec9e40372
Vikas Kumar Gupta <vikasgup074@gmail.com>
2023-10-27 02:08:13 +0530

    Product Design

--- app/src/main/java/com/example/yourfood/frag/HomeFragment.kt
+++ app/src/main/java/com/example/yourfood/frag/HomeFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemChangeListener
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.yourfood.R
+import com.example.yourfood.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding : FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,36 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageList = ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.banner6, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner4, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner5, ScaleTypes.FIT))
+
+        val imageSlider = binding.imageSlider
+        imageSlider.setImageList(imageList)
+        imageSlider.setImageList(imageList, ScaleTypes.FIT)
+
+        imageSlider.setItemClickListener(object : ItemClickListener{
+            override fun doubleClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(position: Int) {
+                val itemPosition = imageList[position]
+                val itemMessage = "Selected Image $position"
+                Toast.makeText(requireContext(), itemMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
+
+
+
+}
+
